fcbe49e8ce789547848e4b9deb3dae286e20a809
amarouane <amarouane@itsc.uah.edu>
2020-10-21 13:47:20 -0500

    restarting MDX service without tty from docker

--- bamboo_build_push.sh
+++ bamboo_build_push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+set -o errexit
+set -o nounset
+set -o pipefail
 export REPO_NAME=mdx
 export AWS_REGION=$bamboo_AWS_REGION
 #access_keys=( $bamboo_ACCESS_KEY_SIT $bamboo_ACCESS_KEY_UAT $bamboo_ACCESS_KEY_PROD)
@@ -14,25 +16,17 @@ account_numbers=( $bamboo_ACCOUNT_NUMBER_SBX )
 prefixes=( $bamboo_PREFIX_SBX )
 
 function stop_mdx_task() {
-  task_arn=$(./aws ecs list-tasks --cluster $1-CumulusECSCluster --service-name $1-MDX --query "taskArns[0]" --region $AWS_REGION | tr -d '"')
-  task_id=${task_arn#*:task/} | tr '\n' ' '
-  echo $task_id
- ./aws ecs stop-task --cluster $1-CumulusECSCluster --task $task_arn --region $AWS_REGION
-
+  task_arn=$(./aws2 ecs list-tasks --cluster $1-CumulusECSCluster --service-name $1-MDX --query "taskArns[0]" --region $AWS_REGION | tr -d '"')
+  ./aws2 ecs stop-task --cluster $1-CumulusECSCluster --task $task_arn --region $AWS_REGION
 }
 
 
 len=${#access_keys[@]}
 
-function check_exit {
-EXIT_STATUS=$?
-if [ $EXIT_STATUS -ne 0 ]; then
-   exit $EXIT_STATUS
-fi
-}
+
 
 docker build -t mdx .
-check_exit
+
 # Copy test results
 docker run --rm -v $PWD/test_results:/opt/mount --entrypoint cp  mdx  /build/test_results/test_metadata_extractor.xml  /opt/mount/test_metadata_extractor.xml
 
@@ -50,18 +44,16 @@ set -o pipefail
 # enable interruption signal handling
 trap - INT TERM
 docker run --rm \
-	-t \$(tty &>/dev/null && echo "-i") \
 	-e "AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID}" \
 	-e "AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY}" \
 	-e "AWS_DEFAULT_REGION=\${AWS_REGION}" \
 	-v "\$(pwd):/project" \
-	maven.earthdata.nasa.gov/aws-cli:2 \
+	amazon/aws-cli:2.0.58  \
 	"\$@"
 EOS
 chmod a+x aws
 docker_image_name=${ACCOUNT_NUMBER}.dkr.ecr.${AWS_REGION}.amazonaws.com/$REPO_NAME
 docker tag mdx $docker_image_name
-check_exit
 ECR=$(./aws ecr get-login --no-include-email --region ${AWS_REGION})
 #./aws ecr create-repository --repository-name $REPO_NAME 2> /dev/null
 echo "creating login temp file"
@@ -71,18 +63,18 @@ echo "login into ecr"
 $(cat ecr.out)
 echo "pushing image to ecr"
 docker push $docker_image_name
-check_exit
+
 stop_mdx_task $prefix
-check_exit
+
 echo "removing temp file"
 rm ecr.out
 docker rmi $docker_image_name
-check_exit
+
 
 done
 
 docker rmi mdx
-check_exit
+
 
 
 
