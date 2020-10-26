bbde0b52053adcb5b37a4fc0979b5baea8431ab5
Lucy Wang <wangl1@uah.edu>
2020-10-26 17:01:44 -0500

    Added isslisg_v1_nqc and isslisg_v1_nrt process and tests

---
+++ test/test_extract_isslisgv1_nqc_nc_metadata.py
@@ -0,0 +1,134 @@
+from os import path
+from unittest import TestCase
+from granule_metadata_extractor.processing.process_isslisgv1 import ExtractIsslisgv1Metadata
+from granule_metadata_extractor.src.generate_echo10_xml import GenerateEcho10XML
+
+#prem metadata for sample file:
+#host=thor,env=ops,project=NOT APPLICABLE,ds=isslisg_v1_nqc,inv=inventory,file=ISS_LIS_BG_V1.0_20201015_NQC_21495.nc,path=nc/2020/1015/ISS_LIS_BG_V1.0_20201015_NQC_21495.nc,size=5853442,start=2020-10-15T01:13:12Z,end=2020-10-15T02:46:04Z,browse=N,checksum=e8b0ee7775e240a615c2b7bd1fcd99ebc7829faa,NLat=55.271427154541016,SLat=-55.536094665527344,WLon=-178.5694122314453,ELon=179.13723754882812,format=netCDF-4
+
+class TestProcessIsslisgv1(TestCase):
+    """
+    Test processing.
+    This will test if metadata will be extracted correctly
+    """
+    granule_name = "ISS_LIS_BG_V1.0_20201015_NQC_21495.nc"
+    input_file = path.join(path.dirname(__file__), f"fixtures/{granule_name}")
+    time_var_key = 'time'
+    lon_var_key = 'lon'
+    lat_var_key = 'lat'
+    time_units = 'units'
+    date_format = '%Y-%m-%dT%H:%M:%SZ'
+    process_dataset = ExtractIsslisgv1Metadata(input_file)
+    md = process_dataset.get_metadata(ds_short_name= 'isslisg_v1_nqc')
+    expected_metadata = {'ShortName': 'isslisg_v1_nqc',
+                         'GranuleUR': granule_name,
+                         'VersionId': '1', 'DataFormat': 'netCDF-4',
+                         }
+
+    def test_1_get_start_date(self):
+        """
+        Testing get correct start date
+        :return:
+        """
+        start_date = self.process_dataset.get_temporal()[0]
+        self.expected_metadata['BeginningDateTime'] = start_date
+
+        self.assertEqual(start_date, "2020-10-15T01:13:12Z")
+
+    def test_2_get_stop_date(self):
+        """
+        Testing get correct end date
+        :return:
+        """
+        stop_date = self.process_dataset.get_temporal()[1]
+        self.expected_metadata['EndingDateTime'] = stop_date
+
+        self.assertEqual(stop_date, "2020-10-15T02:46:04Z")
+
+    def test_3_get_file_size(self):
+        """
+        Test geting the correct file size
+        :return:
+        """
+        file_size = float(self.md['SizeMBDataGranule'])
+        self.expected_metadata['SizeMBDataGranule'] = str(file_size)
+        self.assertEqual(file_size, 5.85)
+
+    def get_wnes(self, index):
+        """
+        A function helper to get North, West, Souh, East
+        :return: wnes[index] where index: west = 0 - north = 1 - east = 2 - south = 3
+        """
+        process_geos = self.process_dataset
+        wnes = process_geos.get_wnes_geometry()
+        return str(round(float(wnes[index]), 3))
+
+    #NLat=55.271427154541016,SLat=-55.536094665527344,WLon=-178.5694122314453,ELon=179.13723754882812
+
+    def test_4_get_north(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        north = self.get_wnes(1)
+        self.expected_metadata['NorthBoundingCoordinate'] = north
+        self.assertEqual(north, '55.271')
+
+    def test_5_get_west(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        west = self.get_wnes(0)
+        self.expected_metadata['WestBoundingCoordinate'] = west
+        self.assertEqual(west, '-178.569')
+
+    def test_6_get_south(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        south = self.get_wnes(3)
+        self.expected_metadata['SouthBoundingCoordinate'] = south
+        self.assertEqual(south, '-55.536')
+
+    def test_7_get_east(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        east = self.get_wnes(2)
+        self.expected_metadata['EastBoundingCoordinate'] = east
+        self.assertEqual(east, '179.137')
+
+    def test_8_get_checksum(self):
+        """
+        Test geting the chucksom of the input file
+        :return: the MD5 string
+        """
+
+        # checksum = self.process_goesrpltavirisng.get_checksum()
+        checksum = self.md['checksum']
+        self.expected_metadata['checksum'] = checksum
+        self.assertEqual(checksum, '5a6ad11d4394f9d27ae20b308a474037')
+
+    def test_9_generate_metadata(self):
+        """
+        Test generating metadata of goesrpltavirisng
+        :return: metadata object 
+        """
+
+        metadata = self.process_dataset.get_metadata(ds_short_name='isslisg_v1_nqc',
+                                                     format='netCDF-4', version='1')
+        for key in self.expected_metadata.keys():
+            self.assertEqual(metadata[key], self.expected_metadata[key])
+
+    def test_a1_generate_echo10(self):
+        """
+        Test generate the echo 10 in tmp folder
+        """
+        self.expected_metadata['OnlineAccessURL'] = "http://localhost.com"
+        echo10xml = GenerateEcho10XML(self.expected_metadata)
+        echo10xml.generate_echo10_xml_file()
+        self.assertTrue(path.exists(f'/tmp/{self.granule_name}.cmr.xml'))
+
---
+++ test/test_extract_isslisgv1_nrt_nc_metadata.py
@@ -0,0 +1,134 @@
+from os import path
+from unittest import TestCase
+from granule_metadata_extractor.processing.process_isslisgv1 import ExtractIsslisgv1Metadata
+from granule_metadata_extractor.src.generate_echo10_xml import GenerateEcho10XML
+
+#prem metadata for sample file:
+#host=thor,env=ops,project=NOT APPLICABLE,ds=isslisg_v1_nrt,inv=inventory,file=ISS_LIS_BG_V1.0_20201026_210255_NRT_21679.nc,path=nc/2020/1026/ISS_LIS_BG_V1.0_20201026_210255_NRT_21679.nc,size=107757,start=2020-10-26T21:03:22Z,end=2020-10-26T21:04:59Z,browse=N,checksum=6d1d753f9a0d0045e988e5cdb376c6d5f53cf3af,NLat=34.162620544433594,SLat=23.287738800048828,WLon=84.44564056396484,ELon=96.73506927490234,format=netCDF-4
+
+class TestProcessIsslisgv1(TestCase):
+    """
+    Test processing.
+    This will test if metadata will be extracted correctly
+    """
+    granule_name = "ISS_LIS_BG_V1.0_20201026_210255_NRT_21679.nc"
+    input_file = path.join(path.dirname(__file__), f"fixtures/{granule_name}")
+    time_var_key = 'time'
+    lon_var_key = 'lon'
+    lat_var_key = 'lat'
+    time_units = 'units'
+    date_format = '%Y-%m-%dT%H:%M:%SZ'
+    process_dataset = ExtractIsslisgv1Metadata(input_file)
+    md = process_dataset.get_metadata(ds_short_name= 'isslisg_v1_nrt')
+    expected_metadata = {'ShortName': 'isslisg_v1_nrt',
+                         'GranuleUR': granule_name,
+                         'VersionId': '1', 'DataFormat': 'netCDF-4',
+                         }
+
+    def test_1_get_start_date(self):
+        """
+        Testing get correct start date
+        :return:
+        """
+        start_date = self.process_dataset.get_temporal()[0]
+        self.expected_metadata['BeginningDateTime'] = start_date
+
+        self.assertEqual(start_date, "2020-10-26T21:03:22Z")
+
+    def test_2_get_stop_date(self):
+        """
+        Testing get correct end date
+        :return:
+        """
+        stop_date = self.process_dataset.get_temporal()[1]
+        self.expected_metadata['EndingDateTime'] = stop_date
+
+        self.assertEqual(stop_date, "2020-10-26T21:04:59Z")
+
+    def test_3_get_file_size(self):
+        """
+        Test geting the correct file size
+        :return:
+        """
+        file_size = float(self.md['SizeMBDataGranule'])
+        self.expected_metadata['SizeMBDataGranule'] = str(file_size)
+        self.assertEqual(file_size, 0.11)
+
+    def get_wnes(self, index):
+        """
+        A function helper to get North, West, Souh, East
+        :return: wnes[index] where index: west = 0 - north = 1 - east = 2 - south = 3
+        """
+        process_geos = self.process_dataset
+        wnes = process_geos.get_wnes_geometry()
+        return str(round(float(wnes[index]), 3))
+
+    #NLat=34.162620544433594,SLat=23.287738800048828,WLon=84.44564056396484,ELon=96.73506927490234
+
+    def test_4_get_north(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        north = self.get_wnes(1)
+        self.expected_metadata['NorthBoundingCoordinate'] = north
+        self.assertEqual(north, '34.163')
+
+    def test_5_get_west(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        west = self.get_wnes(0)
+        self.expected_metadata['WestBoundingCoordinate'] = west
+        self.assertEqual(west, '84.446')
+
+    def test_6_get_south(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        south = self.get_wnes(3)
+        self.expected_metadata['SouthBoundingCoordinate'] = south
+        self.assertEqual(south, '23.288')
+
+    def test_7_get_east(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        east = self.get_wnes(2)
+        self.expected_metadata['EastBoundingCoordinate'] = east
+        self.assertEqual(east, '96.735')
+
+    def test_8_get_checksum(self):
+        """
+        Test geting the chucksom of the input file
+        :return: the MD5 string
+        """
+
+        # checksum = self.process_goesrpltavirisng.get_checksum()
+        checksum = self.md['checksum']
+        self.expected_metadata['checksum'] = checksum
+        self.assertEqual(checksum, '6a7bdf4c1a702f2beeb0308ff18f936a')
+
+    def test_9_generate_metadata(self):
+        """
+        Test generating metadata of goesrpltavirisng
+        :return: metadata object 
+        """
+
+        metadata = self.process_dataset.get_metadata(ds_short_name='isslisg_v1_nrt',
+                                                     format='netCDF-4', version='1')
+        for key in self.expected_metadata.keys():
+            self.assertEqual(metadata[key], self.expected_metadata[key])
+
+    def test_a1_generate_echo10(self):
+        """
+        Test generate the echo 10 in tmp folder
+        """
+        self.expected_metadata['OnlineAccessURL'] = "http://localhost.com"
+        echo10xml = GenerateEcho10XML(self.expected_metadata)
+        echo10xml.generate_echo10_xml_file()
+        self.assertTrue(path.exists(f'/tmp/{self.granule_name}.cmr.xml'))
+
