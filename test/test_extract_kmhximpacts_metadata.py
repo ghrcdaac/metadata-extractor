ab320298926c0e0ccecadd7999077a5ca5cb93fd
Lucy Wang <wangl1@uah.edu>
2021-02-15 15:11:20 -0600

    Added 5 NEXRAD IMPACTS (kjkl, klot, klwx, kmhx, kmkx) processes and tests

---
+++ test/test_extract_kmhximpacts_metadata.py
@@ -0,0 +1,132 @@
+from os import path
+from unittest import TestCase
+from granule_metadata_extractor.processing.process_kmhximpacts import ExtractKmhximpactsMetadata
+from granule_metadata_extractor.src.generate_echo10_xml import GenerateEcho10XML
+
+#prem metadata for sample file:
+#host=thor,env=ops,project=IMPACTS,ds=kmhximpacts,inv=inventory,file=IMPACTS_nexrad_20200212_170433_kmhx.nc,path=20200212/IMPACTS_nexrad_20200212_170433_kmhx.nc,size=194900,start=2020-02-12T17:04:33Z,end=2020-02-12T17:04:39Z,browse=N,checksum=c5dcb1266784361f6fada8073226ae0dbd9f060b,NLat=34.76175308227539,SLat=30.64613914489746,WLon=-80.13714599609375,ELon=-74.93790435791016,format=netCDF-4
+
+class TestProcessKmhximpacts(TestCase):
+    """
+    Test processing.
+    This will test if metadata will be extracted correctly
+    """
+    granule_name = "IMPACTS_nexrad_20200212_170433_kmhx.nc"
+    input_file = path.join(path.dirname(__file__), f"fixtures/{granule_name}")
+    time_var_key = 'time'
+    lon_var_key = 'lon'
+    lat_var_key = 'lat'
+    time_units = 'units'
+    date_format = '%Y-%m-%dT%H:%M:%SZ'
+    process_dataset = ExtractKmhximpactsMetadata(input_file)
+    md = process_dataset.get_metadata(ds_short_name= 'kmhximpacts')
+    expected_metadata = {'ShortName': 'kmhximpacts',
+                         'GranuleUR': granule_name,
+                         'VersionId': '1', 'DataFormat': 'netCDF-4',
+                         }
+
+    def test_1_get_start_date(self):
+        """
+        Testing get correct start date
+        :return:
+        """
+        start_date = self.process_dataset.get_temporal()[0]
+        self.expected_metadata['BeginningDateTime'] = start_date
+
+        self.assertEqual(start_date, "2020-02-12T17:04:33Z")
+
+    def test_2_get_stop_date(self):
+        """
+        Testing get correct end date
+        :return:
+        """
+        stop_date = self.process_dataset.get_temporal()[1]
+        self.expected_metadata['EndingDateTime'] = stop_date
+
+        self.assertEqual(stop_date, "2020-02-12T17:04:39Z")
+
+    def test_3_get_file_size(self):
+        """
+        Test geting the correct file size
+        :return:
+        """
+        file_size = float(self.md['SizeMBDataGranule'])
+        self.expected_metadata['SizeMBDataGranule'] = str(file_size)
+        self.assertEqual(file_size, 0.19)
+
+    def get_wnes(self, index):
+        """
+        A function helper to get North, West, Souh, East
+        :return: wnes[index] where index: west = 0 - north = 1 - east = 2 - south = 3
+        """
+        process_geos = self.process_dataset
+        wnes = process_geos.get_wnes_geometry()
+        return str(round(float(wnes[index]), 3))
+
+    #NLat=34.76175308227539,SLat=30.64613914489746,WLon=-80.13714599609375,ELon=-74.93790435791016
+
+    def test_4_get_north(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        north = self.get_wnes(1)
+        self.expected_metadata['NorthBoundingCoordinate'] = north
+        self.assertEqual(north, '34.762')
+
+    def test_5_get_west(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        west = self.get_wnes(0)
+        self.expected_metadata['WestBoundingCoordinate'] = west
+        self.assertEqual(west, '-80.137')
+
+    def test_6_get_south(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        south = self.get_wnes(3)
+        self.expected_metadata['SouthBoundingCoordinate'] = south
+        self.assertEqual(south, '30.646')
+
+    def test_7_get_east(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        east = self.get_wnes(2)
+        self.expected_metadata['EastBoundingCoordinate'] = east
+        self.assertEqual(east, '-74.938')
+
+    def test_8_get_checksum(self):
+        """
+        Test geting the chucksom of the input file
+        :return: the MD5 string
+        """
+
+        checksum = self.md['checksum']
+        self.expected_metadata['checksum'] = checksum
+        self.assertEqual(checksum, '40483e57a25478d6e44c910e52dfb848')
+
+    def test_9_generate_metadata(self):
+        """
+        Test generating metadata
+        :return: metadata object 
+        """
+
+        metadata = self.process_dataset.get_metadata(ds_short_name='kmhximpacts',
+                                                     format='netCDF-4', version='1')
+        for key in self.expected_metadata.keys():
+            self.assertEqual(metadata[key], self.expected_metadata[key])
+
+    def test_a1_generate_echo10(self):
+        """
+        Test generate the echo 10 in tmp folder
+        """
+        self.expected_metadata['OnlineAccessURL'] = "http://localhost.com"
+        echo10xml = GenerateEcho10XML(self.expected_metadata)
+        echo10xml.generate_echo10_xml_file()
+        self.assertTrue(path.exists(f'/tmp/{self.granule_name}.cmr.xml'))
