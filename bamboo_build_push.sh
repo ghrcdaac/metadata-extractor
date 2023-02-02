8da4ffb68d40a87d915584e9edbede351a1599ce
ecampos333 <ebc0007@uah.edu>
2023-02-01 20:12:22 -0600

    Update build code to contain more debug logging in order to resolve bamboo plan upgrade issue.

--- bamboo_build_push.sh
+++ bamboo_build_push.sh
@@ -3,43 +3,42 @@ source ./common.sh
 set -o nounset
 set -o pipefail
 
-
-access_keys=( $bamboo_AWS_SBX_ACCESS_KEY $bamboo_AWS_SIT_ACCESS_KEY $bamboo_AWS_UAT_ACCESS_KEY $bamboo_AWS_PROD_ACCESS_KEY )
-secret_keys=( $bamboo_AWS_SBX_SECRET_ACCESS_KEY $bamboo_AWS_SIT_SECRET_ACCESS_KEY $bamboo_AWS_UAT_SECRET_ACCESS_KEY $bamboo_AWS_PROD_SECRET_ACCESS_KEY )
-prefixes=( $bamboo_PREFIX_SBX $bamboo_PREFIX_SIT $bamboo_PREFIX_UAT $bamboo_PREFIX_PROD )
-account_numbers=( $bamboo_ACCOUNT_NUMBER_SBX $bamboo_ACCOUNT_NUMBER_SIT $bamboo_ACCOUNT_NUMBER_UAT $bamboo_ACCOUNT_NUMBER_PROD )
-
+access_keys=($bamboo_AWS_SBX_ACCESS_KEY $bamboo_AWS_SIT_ACCESS_KEY $bamboo_AWS_UAT_ACCESS_KEY $bamboo_AWS_PROD_ACCESS_KEY)
+secret_keys=($bamboo_AWS_SBX_SECRET_ACCESS_KEY $bamboo_AWS_SIT_SECRET_ACCESS_KEY $bamboo_AWS_UAT_SECRET_ACCESS_KEY $bamboo_AWS_PROD_SECRET_ACCESS_KEY)
+prefixes=($bamboo_PREFIX_SBX $bamboo_PREFIX_SIT $bamboo_PREFIX_UAT $bamboo_PREFIX_PROD)
+account_numbers=($bamboo_ACCOUNT_NUMBER_SBX $bamboo_ACCOUNT_NUMBER_SIT $bamboo_ACCOUNT_NUMBER_UAT $bamboo_ACCOUNT_NUMBER_PROD)
 
 function build_docker {
-echo $bamboo_GITHUB_REGISTRY_READ_TOKEN_SECRET | docker login ghcr.io -u bamboo_login --password-stdin
-if [[ $(uname -m) == arm64* ]]; then
-  docker_build="docker buildx build --push --platform linux/arm64,linux/amd64 -t"
-else
- docker_build="docker build -t"
-fi
-${docker_build} $1 .
+  echo "Building Docker"
+  echo $bamboo_GITHUB_REGISTRY_READ_TOKEN_SECRET | docker login ghcr.io -u bamboo_login --password-stdin
+  if [[ $(uname -m) == arm64* ]]; then
+    docker_build="docker buildx build --push --platform linux/arm64,linux/amd64 -t"
+  else
+    docker_build="docker build -t"
+  fi
+  ${docker_build} $1 .
 
 }
-
 
 len=${#access_keys[@]}
 
 # Check keys
-for (( i=0; i<$len; i++ ))
-do
+for ((i = 0; i < $len; i++)); do
   export AWS_ACCESS_KEY_ID=${access_keys[$i]}
   export AWS_SECRET_ACCESS_KEY=${secret_keys[$i]}
   aws sts get-caller-identity
-  (($? != 0)) && { printf '%s\n' "Command exited with non-zero. AWS keys invalid"; exit 1; }
+  (($? != 0)) && {
+    printf '%s\n' "Command exited with non-zero. AWS keys invalid"
+    exit 1
+  }
 done
 
 build_docker mdx
 
 # Copy test results
-docker run --rm -v $PWD/test_results:/opt/mount --entrypoint cp  mdx  /var/task/test_results/test_metadata_extractor.xml  /opt/mount/test_metadata_extractor.xml
+docker run --rm -v $PWD/test_results:/opt/mount --entrypoint cp mdx /var/task/test_results/test_metadata_extractor.xml /opt/mount/test_metadata_extractor.xml
 
-for (( i=0; i<$len; i++ ))
-do
+for ((i = 0; i < $len; i++)); do
   export AWS_ACCESS_KEY_ID=${access_keys[$i]}
   export AWS_SECRET_ACCESS_KEY=${secret_keys[$i]}
   export ACCOUNT_NUMBER=${account_numbers[$i]}
@@ -49,9 +48,6 @@ do
   push_to_ecr $ACCOUNT_NUMBER $prefix
   update_lambda_or_skip $ACCOUNT_NUMBER $prefix
 
-
-
 done
 
 docker rmi mdx
-
--- common.sh
+++ common.sh
@@ -3,7 +3,6 @@
 export REPO_NAME=mdx_docker_lambda
 export AWS_REGION=${bamboo_AWS_REGION:-us-west-2}
 
-
 if [[ -z "${AWS_PROFILE}" ]]; then
   ADD_PROFILE=""
 else
@@ -11,23 +10,26 @@ else
 fi
 
 function update_lambda_or_skip() {
-  check_lambda_exist=$(aws lambda get-function --region $AWS_REGION $ADD_PROFILE --function-name $2-$REPO_NAME 2> /dev/null)
-    if [[ ! -n "${check_lambda_exist}" ]]; then
-    echo "NO lambda found ${REPO_NAME} SKIPPING"
-    else
-      update_lambda $1 $2 2> /dev/null
+  echo "Checking Lambda Update Status..."
+  check_lambda_exist=$(aws lambda get-function --region $AWS_REGION $ADD_PROFILE --function-name $2-$REPO_NAME 2>/dev/null)
+  if [[ ! -n "${check_lambda_exist}" ]]; then
+    echo "Lambda ${2}-${REPO_NAME} Does Not Exist SKIPPING UPDATE"
+  else
+    echo "Lambda ${2}-${REPO_NAME} Exists"
+    update_lambda $1 $2 2>/dev/null
   fi
 }
 
-function update_lambda {
-docker_image_name=$1.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME
-aws lambda update-function-code \
---function-name $2-$REPO_NAME \
---image-uri ${docker_image_name}:latest \
---region ${AWS_REGION} \
-$ADD_PROFILE
+function update_lambda() {
+  echo "Updating Lambda ${2}-${REPO_NAME}"
+  docker_image_name=$1.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME
+  aws lambda update-function-code \
+    --function-name $2-$REPO_NAME \
+    --image-uri ${docker_image_name}:latest \
+    --region ${AWS_REGION} \
+    $ADD_PROFILE
+  echo "Lambda ${2}-${REPO_NAME} Updated"
 }
-
 
 function push_to_ecr() {
   # $ACCOUNT_NUMBER = $1
@@ -35,27 +37,28 @@ function push_to_ecr() {
   docker_image_name=$1.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME
   docker tag mdx $docker_image_name
 
+  echo "Signing into ECR"
   aws ecr get-login-password \
-      --region $AWS_REGION \
-  | docker login \
+    --region $AWS_REGION |
+    docker login \
       --username AWS \
       --password-stdin $1.dkr.ecr.$AWS_REGION.amazonaws.com
 
-
-  echo "pushing image to ecr"
+  echo "Pushing Image to ECR"
   docker push $docker_image_name
-
-
+  echo "Removing Image from Local"
   docker rmi $docker_image_name
 }
 
-
 function create_ecr_repo_or_skip() {
-  check_repo_exist=$(aws ecr describe-repositories --region $AWS_REGION --repository-names $REPO_NAME 2> /dev/null)
+  echo "Checking ECR Repo Status..."
+  check_repo_exist=$(aws ecr describe-repositories --region $AWS_REGION --repository-names $REPO_NAME 2>/dev/null)
   if [[ ! -n "${check_repo_exist}" ]]; then
-    echo "we need to create ${REPO_NAME}"
+    echo "Repo creation needed for ${REPO_NAME}"
     aws ecr create-repository --repository-name $REPO_NAME --region $AWS_REGION
     echo "${REPO_NAME} was created"
+  else
+    echo "${REPO_NAME} already exists"
   fi
 
 }
