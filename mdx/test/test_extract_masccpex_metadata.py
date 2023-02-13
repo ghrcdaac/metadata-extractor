f60bd5fc92c8bf32374884a6561887b323ad0e0c
Lucy Wang <lwang@camellia.local>
2023-02-13 14:24:02 -0600

    Added masccpex process and test codes

---
+++ mdx/test/test_extract_masccpex_metadata.py
@@ -0,0 +1,130 @@
+from os import path
+from unittest import TestCase
+from granule_metadata_extractor.processing.process_masccpex import ExtractMasccpexMetadata
+from granule_metadata_extractor.src.generate_umm_g_json import GenerateUmmGJson
+
+#prem metadata for sample file:
+#host=thor,env=ops,project=CPEX,ds=masccpex,inv=inventory,file=CPEX_MASC_Fri_Jun__2_16-17-34_2017.he5,path=20170602/CPEX_MASC_Fri_Jun__2_16-17-34_2017.he5,size=610512,start=2017-06-02T16:57:44Z,end=2017-06-02T17:05:08Z,browse=Y,checksum=2a0a7e88a9fc946409c764e91c6d3d    52c4056d7c,NLat=26.322189331054688,SLat=26.210573196411133,WLon=-80.55681610107422,ELon=-79.9765396118164,format=HDF-5
+class TestProcessMasccpex(TestCase):
+    """
+    Test processing.
+    This will test if metadata will be extracted correctly
+    """
+    granule_name = "CPEX_MASC_Fri_Jun__2_16-17-34_2017.he5"
+    input_file = path.join(path.dirname(__file__), f"fixtures/{granule_name}")
+    time_var_key = 'time'
+    lon_var_key = 'lon'
+    lat_var_key = 'lat'
+    time_units = 'units'
+    date_format = '%Y-%m-%dT%H:%M:%SZ'
+    process_dataset = ExtractMasccpexMetadata(input_file)
+    md = process_dataset.get_metadata(ds_short_name= 'masccpex')
+    expected_metadata = {'ShortName': 'masccpex',
+                         'GranuleUR': granule_name,
+                         'VersionId': '1', 'DataFormat': 'HDF-5',
+                         }
+
+    def test_1_get_start_date(self):
+        """
+        Testing get correct start date
+        :return:
+        """
+        start_date = self.process_dataset.get_temporal()[0]
+        self.expected_metadata['BeginningDateTime'] = start_date
+
+        self.assertEqual(start_date, "2017-06-02T16:57:44Z")
+
+    def test_2_get_stop_date(self):
+        """
+        Testing get correct end date
+        :return:
+        """
+        stop_date = self.process_dataset.get_temporal()[1]
+        self.expected_metadata['EndingDateTime'] = stop_date
+
+        self.assertEqual(stop_date, "2017-06-02T17:05:08Z")
+
+    def test_3_get_file_size(self):
+        """
+        Test geting the correct file size
+        :return:
+        """
+        file_size = float(self.md['SizeMBDataGranule'])
+        self.expected_metadata['SizeMBDataGranule'] = str(file_size)
+        self.assertEqual(file_size, 0.61)
+
+    def get_wnes(self, index):
+        """
+        A function helper to get North, West, Souh, East
+        :return: wnes[index] where index: west = 0 - north = 1 - east = 2 - south = 3
+        """
+        process_geos = self.process_dataset
+        wnes = process_geos.get_wnes_geometry()
+        return str(round(float(wnes[index]), 3))
+
+    #NLat=26.322189331054688,SLat=26.210573196411133,WLon=-80.55681610107422,ELon=-79.9765396118164
+    def test_4_get_north(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        north = self.get_wnes(1)
+        self.expected_metadata['NorthBoundingCoordinate'] = north
+        self.assertEqual(north, '26.322')
+
+    def test_5_get_west(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        west = self.get_wnes(0)
+        self.expected_metadata['WestBoundingCoordinate'] = west
+        self.assertEqual(west, '-80.557')
+
+    def test_6_get_south(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        south = self.get_wnes(3)
+        self.expected_metadata['SouthBoundingCoordinate'] = south
+        self.assertEqual(south, '26.211')
+
+    def test_7_get_east(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        east = self.get_wnes(2)
+        self.expected_metadata['EastBoundingCoordinate'] = east
+        self.assertEqual(east, '-79.977')
+
+    def test_8_get_checksum(self):
+        """
+        Test geting the chucksom of the input file
+        :return: the MD5 string
+        """
+
+        checksum = self.md['checksum']
+        self.expected_metadata['checksum'] = checksum
+        self.assertEqual(checksum, 'f64edc6a06fab221511b1d1aa52d56c4')
+
+    def test_9_generate_metadata(self):
+        """
+        Test generating metadata
+        :return: metadata object 
+        """
+
+        metadata = self.process_dataset.get_metadata(ds_short_name='masccpex',
+                                                     format='HDF-5', version='1')
+        for key in self.expected_metadata.keys():
+            self.assertEqual(metadata[key], self.expected_metadata[key])
+
+    def test_a1_generate_umm_json(self):
+        """
+        Test generate the umm json in tmp folder
+        """
+        self.expected_metadata['OnlineAccessURL'] = "http://localhost.com"
+        umm_json = GenerateUmmGJson(self.expected_metadata)
+        umm_json.generate_umm_json_file()
+        self.assertTrue(path.exists(f'/tmp/{self.granule_name}.cmr.json'))
