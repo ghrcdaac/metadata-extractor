4411b5208dbbf471ed782bade0dc7da85d63803e
camposeddie <eddie.campos@uah.edu>
2023-11-03 16:55:44 -0400

    Add / handling

--- mdx/granule_metadata_extractor/src/helpers/creators/utils/mdx.py
+++ mdx/granule_metadata_extractor/src/helpers/creators/utils/mdx.py
@@ -52,7 +52,7 @@ class MDX:
             Bucket=bucket, Prefix=f"{prefix.rstrip('/')}/")
         for page in page_iterator:
             if page["KeyCount"] < 1:
-                print(f"No objects found at s3://{bucket}/{prefix}/")
+                print(f"No objects found at s3://{bucket}/{prefix.rstrip('/')}/")
                 return obj_list
             for obj in page["Contents"]:
                 obj_list.append(f"s3://{bucket}/{obj['Key']}")
