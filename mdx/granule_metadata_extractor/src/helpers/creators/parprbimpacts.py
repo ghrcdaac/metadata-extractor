e00297aad7ad8ef5e80bf0249651ad5b3b48eacf
Lucy Wang <wangl1@uah.edu>
2024-02-22 15:45:46 -0600

    Create lookup file creator for netCDf-4 files in parprbimpacts

--- mdx/granule_metadata_extractor/src/helpers/creators/parprbimpacts.py
+++ mdx/granule_metadata_extractor/src/helpers/creators/parprbimpacts.py
@@ -1,95 +1,97 @@
-#-----------------------------------------------------------------------------#
-# Assemble needed resources                                                   #
-#-----------------------------------------------------------------------------#
-import os, json, datetime
-from datetime import datetime, date, timedelta
+# create lookup zip for parprbimpacts
+# for all future collections
+from datetime import datetime, timedelta
+from utils.mdx import MDX
+import cProfile
+import time
+import math
+import re
+
 from netCDF4 import Dataset
 import numpy as np
-from hashlib import md5
 
-#-- latest development prefer fix root directory
-data_dir = "/ftp/ops/public/pub/fieldCampaigns/impacts/NCAR_Particle_Probes/data"
-
-metadata = {}
-metadata_ref = {}
-
-def get_nc_metadata(filename):
-    datafile = Dataset(filename)
-    lats = np.array(datafile['LAT'][:])
-    lons = np.array(datafile['LON'][:])
-    sec = np.array(datafile['time'][:])
-    ref_time = datetime.strptime(datafile.FlightDate,'%m/%d/%Y') 
-
-    #Extract metadata from netCDF-4 data files
-    start, end = [ref_time+timedelta(seconds=sec.min()),
-                              ref_time+timedelta(seconds=sec.max())]
-    nlat, slat, elon, wlon = [np.nanmax(lats),
-                              np.nanmin(lats),
-                              np.nanmax(lons),
-                              np.nanmin(lons)]
-
-    #create netCDF4 metadata for json input
-    key0 = os.path.basename(filename)
-    metadata[key0] = {}
-    metadata[key0]["temporal"] = [x.strftime('%Y-%m-%dT%H:%M:%SZ') for x in [start,end]]
-    metadata[key0]["wnes_geometry"] = [str(round(x,3)) for x in [wlon,nlat,elon,slat]]
-    metadata[key0]['SizeMBDataGranule'] = str(round(1E-6 * os.path.getsize(filename), 2))
-    with open(filename, 'rb') as file:
-        metadata[key0]['checksum'] = md5(file.read()).hexdigest()
-    metadata[key0]['format'] = 'netCDF-4'
-
-    #collect metadata that will be assigned to PNG
-    rr = {}
-    key = filename.split('/')[-1].split('_')[2] #i.e., 20200108
-    if key not in metadata_ref.keys():
-       rr['start'], rr['end'] = [start,end]
-       rr['NLat'], rr['SLat'], rr['ELon'], rr['WLon'] = [nlat,slat,elon,wlon] 
-       rr['Probes'] = datafile.ProbeName
-    else:
-       #If metadata_ref[key] already exists,update values
-       rr['start'] = min(metadata_ref[key]['start'],start)
-       rr['end'] = max(metadata_ref[key]['end'],end)
-       rr['NLat'] = max(metadata_ref[key]['NLat'],nlat)
-       rr['SLat'] = min(metadata_ref[key]['SLat'],slat)
-       rr['ELon'] = max(metadata_ref[key]['ELon'],elon)
-       rr['WLon'] = min(metadata_ref[key]['WLon'],wlon)
-       rr['Probes'] = ''.join([metadata_ref[key]['Probes'],' ',datafile.ProbeName])
-    metadata_ref[key] = rr
-
-    datafile.close()
-
-def get_png_metadata(filename):
-    key = filename.split('/')[-1].split('_')[2]
-    nlat = metadata_ref[key]["NLat"]
-    slat = metadata_ref[key]["SLat"]
-    elon = metadata_ref[key]["ELon"]
-    wlon = metadata_ref[key]["WLon"]
-    start = metadata_ref[key]["start"]
-    end = metadata_ref[key]["end"]
-
-    key0 = os.path.basename(filename)
-    metadata[key0] = {}
-    metadata[key0]["temporal"] = [x.strftime('%Y-%m-%dT%H:%M:%SZ') for x in [start,end]]
-    metadata[key0]["wnes_geometry"] = [str(round(x,3)) for x in [wlon,nlat,elon,slat]]
-    metadata[key0]['SizeMBDataGranule'] = str(round(1E-6 * os.path.getsize(filename), 2))
-    with open(filename, 'rb') as file:
-        metadata[key0]['checksum'] = md5(file.read()).hexdigest()
-    metadata[key0]['format'] = 'PNG'
+short_name = "parprbimpacts"
+provider_path = "parprbimpacts/fieldCampaigns/impacts/NCAR_Particle_Probes/data/nc/"
+file_type = "netÇDF-4"
 
 
-for root, dirs, files in os.walk(data_dir):
-    dirs.sort()
-    for filename in sorted(files):
+class MDXProcessing(MDX):
+
+    def __init__(self):
+        super().__init__()
+
+    def process(self, filename, file_obj_stream) -> dict:
+        """
+        Individual collection processing logic for spatial and temporal 
+        metadata extraction
+        :param filename: name of file to process
+        :type filename: str
+        :param file_obj_stream: file object stream to be processed
+        :type file_obj_stream: botocore.response.StreamingBody
+        """
         if filename.endswith('.nc'):
-           #print(os.path.join(root, filename))
-           get_nc_metadata(os.path.join(root, filename))
+           return self.get_nc_metadata(filename, file_obj_stream)
 
-for root, dirs, files in os.walk(data_dir):
-    dirs.sort()
-    for filename in sorted(files):
-        if filename.endswith('.tar'):
-           #print(os.path.join(root, filename))
-           get_png_metadata(os.path.join(root, filename))
 
-with open('parprbimpactsRefData.json', 'w') as fp:
-     json.dump(metadata, fp)
+    def get_nc_metadata(self, filename, file_obj_stream):
+        """
+        Extract temporal and spatial metadata from netCDF-3 files
+        """
+        datafile = Dataset("in-mem-file", mode='r', memory=file_obj_stream.read())
+        lats = np.array(datafile['LAT'][:])
+        lons = np.array(datafile['LON'][:])
+        sec = np.array(datafile['time'][:])
+        ref_time = datetime.strptime(datafile.FlightDate,'%m/%d/%Y')
+
+        #Extract metadata from netCDF-4 data files
+        start_time, end_time = [ref_time+timedelta(seconds=sec.min()),
+                                ref_time+timedelta(seconds=sec.max())]
+        north, south, east, west = [np.nanmax(lats),
+                                    np.nanmin(lats),
+                                    np.nanmax(lons),
+                                    np.nanmin(lons)]
+        datafile.close()
+        return {
+            "start": start_time,
+            "end": end_time,
+            "north": north,
+            "south": south,
+            "east": east,
+            "west": west,
+            "format": file_type
+        }
+
+
+
+    def read_metadata_diameter020(self):
+        """
+        Extracts temporal and spatial metadata from diameter020 files
+        """
+        start_time = datetime(2020, 1, 15, 1, 37, 6)
+        end_time = datetime(2020,2,28,20,41,44)
+        north, south, east, west = [37.954319999999996, 37.919380000000004,
+                                    -75.44618, -75.49116000000001]
+        return {
+            "start": start_time,
+            "end": end_time,
+            "north": north,
+            "south": south,
+            "east": east,
+            "west": west,
+            "format": file_type
+        }
+
+
+    def main(self):
+        # start_time = time.time()
+        self.process_collection(short_name, provider_path)
+        # elapsed_time = time.time() - start_time
+        # print(f"Elapsed time in seconds: {elapsed_time}")
+        self.shutdown_ec2()
+
+
+if __name__ == '__main__':
+    MDXProcessing().main()
+    # The below can be use to run a profiler and see which functions are
+    # taking the most time to process
+    # cProfile.run('MDXProcessing().main()', sort='tottime')
