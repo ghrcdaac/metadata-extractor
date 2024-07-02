2a05ff7ede76236bf798ebba50b1ecc8a2ae37e5
Lucy Wang <wangl1@uah.edu>
2024-07-02 12:18:29 -0500

    Added process,test and lookup zip creator codes for glmcierra

---
+++ mdx/granule_metadata_extractor/src/helpers/creators/glmcierra.py
@@ -0,0 +1,89 @@
+# create lookup zip for glmcierra 
+# for all future collections
+from datetime import datetime, timedelta
+from utils.mdx import MDX
+import cProfile
+import time
+import math
+import re
+
+from netCDF4 import Dataset
+import numpy as np
+
+short_name = "glmcierra"
+provider_path = "glmcierra/"
+file_type = "netCDF-4"
+
+file_excluded = ['OR_GLM-L2-CIERRA-DB_GOES-EAST_s20192931845000.nc',
+                 'OR_GLM-L2-CIERRA-DB_GOES-EAST_s20193132345000.nc',
+                 'OR_GLM-L2-CIERRA-DB_GOES-WEST_s20203590215000.nc',
+                 'OR_GLM-L2-CIERRA-DB_GOES-WEST_s20203591600000.nc',
+                 'OR_GLM-L2-CIERRA-DB_GOES-WEST_s20210122000000.nc']
+
+class MDXProcessing(MDX):
+
+    def __init__(self):
+        super().__init__()
+
+    def process(self, filename, file_obj_stream) -> dict:
+        """
+        Individual collection processing logic for spatial and temporal 
+        metadata extraction
+        :param filename: name of file to process
+        :type filename: str
+        :param file_obj_stream: file object stream to be processed
+        :type file_obj_stream: botocore.response.StreamingBody
+        """
+        return self.get_nc_metadata(filename, file_obj_stream)
+
+    def get_nc_metadata(self, filename, file_obj_stream):
+        """
+        Extract temporal and spatial metadata from netCDF-4 files
+        """
+        print(filename)
+        datafile = Dataset("in-mem-file", mode='r', memory=file_obj_stream.read())
+        ftype = datafile.file_format
+        if ftype.startswith('NETCDF3'):
+            file_type = "netCDF-3"
+        else:
+            file_type = "netCDF-4"
+
+        lats = np.array(datafile['FLASH_LAT'][:])
+        lons = np.array(datafile['FLASH_LON'][:])
+
+        north, south, east, west = [np.nanmax(lats),
+                                    np.nanmin(lats),
+                                    np.nanmax(lons),
+                                    np.nanmin(lons)]
+        if filename.split('/')[-1] in file_excluded:
+            #assign summary metadata 
+            #"north": 57.267, "south": -57.312, "east": 180.0, "west": -180.0
+            north, south, east, west = [57.267, -57.312, 180.0, -180.0]
+
+        start_time = datetime.strptime(datafile.TIME_COVERAGE_START,'%Y-%m-%d %H:%M:%SZ')
+        end_time = datetime.strptime(datafile.TIME_COVERAGE_END,'%Y-%m-%d %H:%M:%SZ')
+        datafile.close()
+        return {
+            "start": start_time,
+            "end": end_time,
+            "north": north,
+            "south": south,
+            "east": east,
+            "west": west,
+            "format": file_type
+        }
+
+
+    def main(self):
+        # start_time = time.time()
+        self.process_collection(short_name, provider_path)
+        # elapsed_time = time.time() - start_time
+        # print(f"Elapsed time in seconds: {elapsed_time}")
+        self.shutdown_ec2()
+
+
+if __name__ == '__main__':
+    MDXProcessing().main()
+    # The below can be use to run a profiler and see which functions are
+    # taking the most time to process
+    # cProfile.run('MDXProcessing().main()', sort='tottime')
---
+++ mdx/test/test_extract_lookup_metadata_glmcierra.py
@@ -0,0 +1,118 @@
+from os import path
+from unittest import TestCase
+from granule_metadata_extractor.processing.process_lookup import ExtractLookupMetadata
+from granule_metadata_extractor.src.generate_umm_g_json import GenerateUmmGJson
+
+#prem metadata for sample file:
+#OR_GLM-L2-CIERRA-DB_GOES-EAST_s20230901400000.nc, north,south,east,west = 44.454403, -42.27351, -26.374033, -124.40529, start,end = '2023-03-31 14:00:00Z', '2023-03-31 14:14:59Z'
+class TestProcessLookup(TestCase):
+    """
+    Test lookup collection metadata extraction
+    """
+    granule_name = "OR_GLM-L2-CIERRA-DB_GOES-EAST_s20230901400000.nc"
+    input_file = path.join(path.dirname(__file__), f"fixtures/{granule_name}")
+    time_var_key = 'time'
+    lon_var_key = 'lon'
+    lat_var_key = 'lat'
+    time_units = 'units'
+    date_format = '%Y-%m-%dT%H:%M:%SZ'
+    process_instance = ExtractLookupMetadata(input_file)
+    md = process_instance.get_metadata(ds_short_name= 'glmcierra', format="Not Provided")
+    expected_metadata = {'ShortName': 'glmcierra',
+                         'GranuleUR': granule_name,
+                         'VersionId': '1', 'DataFormat': 'netCDF-4',
+                         }
+
+    def test_1_get_start_date(self):
+        """
+        Testing get correct start date
+        :return:
+        """
+        start_date = self.md['BeginningDateTime']
+        self.expected_metadata['BeginningDateTime'] = start_date
+
+        self.assertEqual(start_date, "2023-03-31T14:00:00Z")
+
+    def test_2_get_stop_date(self):
+        """
+        Testing get correct end date
+        :return:
+        """
+        stop_date = self.md['EndingDateTime']
+        self.expected_metadata['EndingDateTime'] = stop_date
+      
+        self.assertEqual(stop_date, "2023-03-31T14:14:59Z")
+
+    def str_to_num(self, s):
+        """
+        Pars string to either int or float depending on value
+        """
+        try:
+            return int(s)
+        except ValueError:
+            return float(s)
+
+    def test_3_get_file_size(self):
+        """
+        Test geting the correct file size
+        :return:
+        """
+        file_size = self.str_to_num(self.md['SizeMBDataGranule'])
+        self.expected_metadata['SizeMBDataGranule'] = str(file_size)
+        self.assertEqual(file_size, 0)
+
+    #north,south,east,west = 44.454403, -42.27351, -26.374033, -124.40529
+    def test_4_get_north(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        north = self.md['NorthBoundingCoordinate']
+        self.expected_metadata['NorthBoundingCoordinate'] = north
+        self.assertEqual(north, '44.454')
+
+    def test_5_get_west(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        west = self.md['WestBoundingCoordinate']
+        self.expected_metadata['WestBoundingCoordinate'] = west
+        self.assertEqual(west, '-124.405')
+
+    def test_6_get_south(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        south = self.md['SouthBoundingCoordinate']
+        self.expected_metadata['SouthBoundingCoordinate'] = south
+        self.assertEqual(south, '-42.274')
+
+    def test_7_get_east(self):
+        """
+        Test geometry metadata
+        :return:
+        """
+        east = self.md['EastBoundingCoordinate']
+        self.expected_metadata['EastBoundingCoordinate'] = east
+        self.assertEqual(east, '-26.374')
+
+    def test_8_generate_metadata(self):
+        """
+        Test generating metadata of legacy collection
+        :return: metadata object 
+        """
+        metadata = self.process_instance.get_metadata(ds_short_name='glmcierra',
+                                                     format='Not provided', version='1')
+        for key in self.expected_metadata.keys():
+            self.assertEqual(metadata[key], self.expected_metadata[key])
+
+    def test_9_generate_umm_json(self):
+        """
+        Test generate the umm json in tmp folder
+        """
+        self.expected_metadata['OnlineAccessURL'] = "http://localhost.com"
+        umm_json = GenerateUmmGJson(self.expected_metadata)
+        umm_json.generate_umm_json_file()
+        self.assertTrue(path.exists(f'/tmp/{self.granule_name}.cmr.json'))
