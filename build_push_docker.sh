d1c910bf25afd231d56abe313ca90b06ba1a2c91
camposeddie <eddie.campos@uah.edu>
2024-07-18 10:17:27 -0500

    Add mac specific docker build command

--- build_push_docker.sh
+++ build_push_docker.sh
@@ -31,9 +31,13 @@ read -rp 'AWS_PROFILE: ' AWS_PROFILE
 read -rp 'Stack Prefix: ' STACK_PREFIX
 AWS_ACCOUNT_ID=$(get_account_id $AWS_PROFILE)
 
-
-docker build -t ghcr.io/ghrcdaac/mdx:base ./mdx_base
-docker build -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/$REPO_NAME .
+if [[ $(uname -m) == arm64* ]]; then
+  docker_build="docker buildx build --push --platform linux/arm64,linux/amd64 -t"
+else
+  docker_build="docker build -t"
+fi
+${docker_build} ghcr.io/ghrcdaac/mdx:base ./mdx_base
+${docker_build} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/$REPO_NAME .
 check_exit
 
 create_ecr_repo_or_skip $AWS_ACCOUNT_ID $STACK_PREFIX
