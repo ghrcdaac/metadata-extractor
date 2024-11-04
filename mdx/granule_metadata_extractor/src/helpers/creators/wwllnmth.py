07a1b8efd1214466fc6c7b0127cc9311aa6ca13e
Lucy Wang <wangl1@uah.edu>
2024-11-04 15:13:45 -0600

    Created wwllnmth lookup zip creator code wwllnmth.py

---
+++ mdx/granule_metadata_extractor/src/helpers/creators/wwllnmth.py
@@ -0,0 +1,75 @@
+# create lookup zip for wwllnmth
+# for all future collections
+from datetime import datetime, timedelta
+from utils.mdx import MDX
+import cProfile
+import time
+import math
+import re
+
+from netCDF4 import Dataset
+import numpy as np
+
+short_name = "wwllnmth"
+provider_path = "wwllnmth__1/"
+file_type = "netCDF-4"
+
+class MDXProcessing(MDX):
+
+    def __init__(self):
+        super().__init__()
+
+    def process(self, filename, file_obj_stream) -> dict:
+        """
+        Individual collection processing logic for spatial and temporal 
+        metadata extraction
+        :param filename: name of file to process
+        :type filename: str
+        :param file_obj_stream: file object stream to be processed
+        :type file_obj_stream: botocore.response.StreamingBody
+        """
+        return self.get_nc_metadata(filename, file_obj_stream)
+
+
+    def get_nc_metadata(self, filename, file_obj_stream):
+        """
+        Extract temporal and spatial metadata from netCDF-4 files
+        """
+        data = Dataset("in-mem-file", mode='r', memory=file_obj_stream.read())
+        lats = np.array(data['lat'][:])
+        lons = np.array(data['lon'][:])
+        north, south, east, west = [np.nanmax(lats),
+                                    np.nanmin(lats),
+                                    np.nanmax(lons),
+                                    np.nanmin(lons)]
+
+        #filename example: WWLLN_th_2013.nc
+        data_year = int(filename.split('/')[-1].split('.nc')[0].split('_')[-1])
+        start_time = datetime(data_year,1,1)
+        end_time = datetime(data_year,12,31,23,59,59)
+
+        data.close()
+        return {
+            "start": start_time,
+            "end": end_time,
+            "north": north,
+            "south": south,
+            "east": east,
+            "west": west,
+            "format": file_type
+        }
+
+
+    def main(self):
+        # start_time = time.time()
+        self.process_collection(short_name, provider_path)
+        # elapsed_time = time.time() - start_time
+        # print(f"Elapsed time in seconds: {elapsed_time}")
+        self.shutdown_ec2()
+
+
+if __name__ == '__main__':
+    MDXProcessing().main()
+    # The below can be use to run a profiler and see which functions are
+    # taking the most time to process
+    # cProfile.run('MDXProcessing().main()', sort='tottime')
