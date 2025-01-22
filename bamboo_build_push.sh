e4f166000e5a30cff610b943ba963b04410bd7bc
Michael Hall <mlh0079@uah.edu>
2025-01-22 12:02:42 -0600

    - The mdx lambda in each account will now be updated.

--- bamboo_build_push.sh
+++ bamboo_build_push.sh
@@ -3,10 +3,12 @@ source ./common.sh
 set -o nounset
 set -o pipefail
 
-access_keys=( $bamboo_AWS_PROD_ACCESS_KEY )
-secret_keys=( $bamboo_AWS_PROD_SECRET_ACCESS_KEY )
-prefixes=( $bamboo_PREFIX_PROD )
-account_numbers=( $bamboo_ACCOUNT_NUMBER_PROD )
+# PROD is expected to be first in these arrays, do not change the order
+access_keys=( $bamboo_AWS_PROD_ACCESS_KEY $bamboo_AWS_UAT_ACCESS_KEY $bamboo_AWS_SIT_ACCESS_KEY $bamboo_AWS_SBX_ACCESS_KEY )
+access_keys_len=${#access_keys[@]}
+secret_keys=( $bamboo_AWS_PROD_SECRET_ACCESS_KEY $bamboo_AWS_UAT_SECRET_ACCESS_KEY $bamboo_AWS_SIT_SECRET_ACCESS_KEY $bamboo_AWS_SBX_SECRET_ACCESS_KEY )
+prefixes=( $bamboo_PREFIX_PROD $bamboo_PREFIX_UAT $bamboo_PREFIX_SIT $bamboo_PREFIX_SBX )
+account_numbers=( $bamboo_ACCOUNT_NUMBER_PROD $bamboo_ACCOUNT_NUMBER_UAT $bamboo_ACCOUNT_NUMBER_SIT $bamboo_ACCOUNT_NUMBER_SBX )
 
 function build_docker {
   echo "Building Docker"
@@ -20,10 +22,8 @@ function build_docker {
 
 }
 
-len=${#access_keys[@]}
-
 # Check keys
-for ((i = 0; i < $len; i++)); do
+for ((i = 0; i < $access_keys_len; i++)); do
   export AWS_ACCESS_KEY_ID=${access_keys[$i]}
   export AWS_SECRET_ACCESS_KEY=${secret_keys[$i]}
   aws sts get-caller-identity
@@ -38,16 +38,18 @@ build_docker mdx
 # Copy test results
 docker run --rm -v $PWD/test_results:/opt/mount --entrypoint cp mdx /var/task/test_results/test_metadata_extractor.xml /opt/mount/test_metadata_extractor.xml
 
-for ((i = 0; i < $len; i++)); do
+# For each account update mdx lambda lambda
+for ((i = 0; i < $access_keys_len; i++)); do
   export AWS_ACCESS_KEY_ID=${access_keys[$i]}
   export AWS_SECRET_ACCESS_KEY=${secret_keys[$i]}
   export ACCOUNT_NUMBER=${account_numbers[$i]}
   export prefix=${prefixes[$i]}
-  create_ecr_repo_or_skip
-  # Push mdx to all account's ecr
-  push_to_ecr $ACCOUNT_NUMBER $prefix
-  update_lambda_or_skip $ACCOUNT_NUMBER $prefix
-
+  if [ $i == 0 ]; then
+    # Push mdx to prod ECR
+    create_ecr_repo_or_skip
+    push_to_ecr ${account_numbers[0]} ${prefixes[0]}
+  fi
+  update_lambda_or_skip ${account_numbers[i]} ${prefixes[i]}
 done
 
 docker rmi mdx
