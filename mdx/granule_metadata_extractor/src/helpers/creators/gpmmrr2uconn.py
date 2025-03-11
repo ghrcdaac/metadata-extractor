07a6dd7dd04da22fbeb3cf3a36a404a5a61e04f8
EC2 Default User <ec2-user@ip-10-11-128-133.us-west-2.compute.internal>
2025-03-11 14:20:08 +0000

    Updated gpmmrr2uconn.py and created gpmmrr2uconn.zip

--- mdx/granule_metadata_extractor/src/helpers/creators/gpmmrr2uconn.py
+++ mdx/granule_metadata_extractor/src/helpers/creators/gpmmrr2uconn.py
@@ -9,13 +9,16 @@ import re
 from netCDF4 import Dataset
 import numpy as np
 
+import io
+import zipfile
+
 short_name = "gpmmrr2uconn"
-provider_path = "gpmmrr2uconn/MRR2/"
+provider_path = "gpmmrr2uconn/"
 
 #MRR2: (41.80778N, -72.29389E), 2021-2022.
 #MRR_PRO: (41.81778N, -72.2575E), 2022-2023 and 2023-2024
 MRR2={'lat':41.80778,'lon':-72.29389}
-MRR_PRO={'lat':41.81778,'lon':-72.2575'}
+MRR_PRO={'lat':41.81778,'lon':-72.2575}
 
 class MDXProcessing(MDX):
 
@@ -32,23 +35,53 @@ class MDXProcessing(MDX):
         :param file_obj_stream: file object stream to be processed
         :type file_obj_stream: botocore.response.StreamingBody
         """
-        return self.get_nc_metadata(filename, file_obj_stream)
+        if filename.endswith('.nc'):
+           self.lat = MRR2['lat']
+           self.lon = MRR2['lon']
+           return self.get_mrr2_metadata(filename, file_obj_stream)
+        else: #MRR_PRO, zip file
+           self.lat = MRR_PRO['lat']
+           self.lon = MRR_PRO['lon']
+           return self.get_mrrpro_metadata(filename, file_obj_stream)
+
+    def get_mrrpro_metadata(self,filename,file_obj_stream):
+        with zipfile.ZipFile(io.BytesIO(file_obj_stream.read())) as z:
+             files = z.namelist()
+        start_time_list = []
+        for file in files:
+            #IMPACTS_D3R_mrrpro_c01_mom_20230419_11.nc     
+            tkn = ''.join(file.split('.nc')[0].split('_')[-2:]) #i.e., 20230419,11
+            utc = datetime.strptime(tkn,'%Y%m%d%H')
+            start_time_list.append(utc)
+
+        start_time = min(start_time_list)
+        end_time = max(start_time_list) + timedelta(minutes = 59)
+
+        north, south, east, west = [self.lat+0.01, self.lat-0.01,
+                                    self.lon+0.01, self.lon-0.01]
+
+        return {
+            "start": start_time,
+            "end": end_time,
+            "north": north,
+            "south": south,
+            "east": east,
+            "west": west,
+            "format": self.file_type
+        }
 
 
-    def get_nc_metadata(self, filename, file_obj_stream):
+            
+    def get_mrr2_metadata(self, filename, file_obj_stream):
         """
         Extract temporal and spatial metadata from netCDF-3 files
         """
         datafile = Dataset("in-mem-file", mode='r', memory=file_obj_stream.read())
-        starttime = datetime(1970,1,1) + timedelta(seconds=int(datafile['time'][:].min()))
-        endtime = datetime(1970,1,1) + timedelta(seconds=int(datafile['time'][:].max()))
+        start_time = datetime(1970,1,1) + timedelta(seconds=int(datafile['time'][:].min()))
+        end_time = datetime(1970,1,1) + timedelta(seconds=int(datafile['time'][:].max()))
 
-        #Extract metadata from netCDF-3 data files
-        lat = MRR2['lat']
-        lon = MRR2['lon']
-
-        north, south, east, west = [lat+0.01, lat-0.01,
-                                    lon+0.01, lon-0.01]
+        north, south, east, west = [self.lat+0.01, self.lat-0.01,
+                                    self.lon+0.01, self.lon-0.01]
 
         datafile.close()
         return {
