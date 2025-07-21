7af59150caf8692d66c2b54abeb9b97b7f3a682c
Lucy Wang <wangl1@uah.edu>
2025-07-21 11:26:47 -0500

    Added crsaloft.py

--- mdx/granule_metadata_extractor/src/helpers/creators/cossiraloft.py
+++ mdx/granule_metadata_extractor/src/helpers/creators/cossiraloft.py
@@ -1,4 +1,4 @@
-# create lookup zip for crsaloft
+# create lookup zip for cossiraloft
 # for all future collections
 from datetime import datetime, timedelta
 from utils.mdx import MDX
@@ -9,9 +9,9 @@ import re
 from netCDF4 import Dataset
 import numpy as np
 
-short_name = "crsaloft"
-provider_path = "aloftcrs/"
-file_type = "HDF-5"
+short_name = "cossiraloft"
+provider_path = "aloftcossir/"
+file_type = "netCDF-4"
 
 
 class MDXProcessing(MDX):
@@ -28,29 +28,40 @@ class MDXProcessing(MDX):
         :param file_obj_stream: file object stream to be processed
         :type file_obj_stream: botocore.response.StreamingBody
         """
-        return self.get_hdf5_metadata(filename, file_obj_stream)
+        return self.get_nc_metadata(filename, file_obj_stream)
 
 
-    def get_hdf5_metadata(self, filename, file_obj_stream):
+    def get_nc_metadata(self, filename, file_obj_stream):
         """
-        Extract temporal and spatial metadata from HDF-5 files
+        Extract temporal and spatial metadata from netCDF-4 files
         """
-        h5 = Dataset("in-mem-file", mode='r', memory=file_obj_stream.read())
-        navgrp = h5.groups['Navigation']
-        navdatagroup = navgrp.groups['Data']
-        lat = navdatagroup.variables['Latitude'][:]
-        lon = navdatagroup.variables['Longitude'][:]
-        tgrp = h5.groups['Time']
-        tdatagrp = tgrp.groups['Data']
-        tm = tdatagrp.variables['TimeUTC'][:]
+        nc = Dataset("in-mem-file", mode='r', memory=file_obj_stream.read())
+        lat = np.array(nc['Latitude'])
+        lon = np.array(nc['Longitude'])
+        year = np.array(nc['Year'])
+        mon = np.array(nc['Month'])
+        day = np.array(nc['DayOfMonth'])
+        hr = np.array(nc['Hour'])
+        mn = np.array(nc['Minute'])
+        sec = np.array(nc['Second'])
 
+        #set missing values in lat and lon to np.nan
+        lat[lat==-999.0] = np.nan
+        lon[lon==-999.0] = np.nan
         north, south, east, west = [np.nanmax(lat), np.nanmin(lat),
                                     np.nanmax(lon), np.nanmin(lon)]
 
-        start_time = datetime(1970,1,1) + timedelta(seconds=np.nanmin(tm))
-        end_time = datetime(1970,1,1) + timedelta(seconds=np.nanmax(tm))
+        start_time, end_time = [datetime(2100, 1, 1), datetime(1900, 1, 1)]
+        for i in range(sec.shape[0]):
+            for j in range(sec.shape[1]):
+                if int(year[i,j]) != -999.0:
+                    total_sec = int(hr[i,j]*3600 + mn[i,j]*60 + sec[i,j])
+                    dt = datetime(int(year[i,j]), int(mon[i,j]), int(day[i,j])) + \
+                         timedelta(seconds=total_sec)
+                    start_time = min(dt, start_time)
+                    end_time = max(dt, end_time)
 
-        h5.close()
+        nc.close()
         return {
             "start": start_time,
             "end": end_time,
