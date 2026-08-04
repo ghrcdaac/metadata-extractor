adb5d3bc5c468be18a94f5bd030ba3f3cfcb54a0
jrichardson <richarjt@uah.edu>
2026-08-04 13:06:36 -0500

    Initial add.

---
+++ mdx/granule_metadata_extractor/src/helpers/creators/er2navtc4.py
@@ -0,0 +1,71 @@
+from datetime import datetime, time, timedelta, timezone
+from utils.mdx import MDX
+from utils.ames import open_ames_1001
+from typing import Any
+
+short_name = "er2navtc4"
+provider_path = "ec2navtc4/"
+
+
+class MDXProcessing(MDX):
+
+    def __init__(self):
+        super().__init__()
+        self.nav_lookup = {}
+
+    def main(self):
+        self.process_collection(short_name, provider_path)
+        # self.shutdown_ec2()
+
+    def process(self, filename: str, stream) -> dict[str, Any]:
+
+        # PDF files in this dataset contain no extractable data
+        if not filename.endswith('.txt'):
+            return {}
+
+        start_time = datetime.now()
+        end_time = 0
+        max_lon = 180
+        min_lon = -180
+        max_lat = 90
+        min_lat = -90
+
+        with open_ames_1001(filename) as (header, records):
+
+            latitude_index = header.variable_names.index(
+                'INU latitude (deg'
+            )
+            longitude_index = header.variable_names.index(
+                'INU longitude (deg)'
+            )
+            beginning_of_day = datetime.combine(
+                header.data_date,
+                time.min,
+                tzinfo=timezone.utc,
+            )
+
+            for record in records:
+                timestamp = beginning_of_day + timedelta(
+                    seconds=record.independent
+                )
+                latitude = record.values[latitude_index]
+                longitude = record.values[longitude_index]
+
+                min_lat = min(min_lat, latitude)
+                max_lat = max(max_lat, latitude)
+                min_lon = min(min_lon, longitude)
+                max_lon = max(max_lon, longitude)
+                start_time = min(start_time, timestamp)
+                end_time = max(end_time, timestamp)
+
+                print(timestamp, latitude, longitude)
+
+        return {
+            "start": start_time,
+            "end": end_time,
+            "north": max_lat,
+            "south": min_lat,
+            "east": max_lon,
+            "west": min_lon,
+            "format": "ASCII"
+        }
