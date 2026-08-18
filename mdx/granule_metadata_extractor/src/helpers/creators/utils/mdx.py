7ff6869b1895dfbf3120276d1f3ff966e27ead9b
Joseph T. Richardson <joseph.t.richardson@gmail.com>
2026-08-18 12:57:03 -0500

    Avoid a KeyError when no S3 page contents are returned.

--- mdx/granule_metadata_extractor/src/helpers/creators/utils/mdx.py
+++ mdx/granule_metadata_extractor/src/helpers/creators/utils/mdx.py
@@ -241,9 +241,12 @@ class MDX:
 
         # Get s3uri of all objects at s3 prefix
         collection_lookup = {}
-        count = 0
+        count = 0   
         st = time.time()
         for page in self.get_page_iterator(provider_path):
+            if "Contents" not in page:
+                # Avoid a KeyError if no contents are returned
+                continue
             with concurrent.futures.ProcessPoolExecutor(max_workers=max_concurrent) as executor:
                 futures = []                
                 for obj in page["Contents"]:
