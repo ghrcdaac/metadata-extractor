ddabcebe5c45158d28a75e0df33a46c5495949d2
Joseph T. Richardson <joseph.t.richardson@gmail.com>
2026-09-16 16:14:27 -0500

    Process extractor for auslma.

---
+++ mdx/granule_metadata_extractor/processing/process_auslma.py
@@ -0,0 +1,96 @@
+from ..src.extract_ascii_metadata import ExtractASCIIMetadata
+from datetime import datetime, timedelta
+import os
+import re
+import gzip
+
+
+class ExtractAuslmaMetadata(ExtractASCIIMetadata):
+    """
+    A class to extract auslma granule metadata
+    """
+    start_time = datetime(2100, 1, 1)
+    end_time = datetime(1900, 1, 1)
+    north = -90.0
+    south = 90.0
+    east = -180.0
+    west = 180.0
+
+    def __init__(self, file_path):
+        super().__init__(file_path)
+        self.file_path = file_path
+        self.file_name = os.path.basename(file_path)
+        self.get_variables_min_max()
+
+    def get_variables_min_max(self, **kwargs):
+        """
+        Extracts temporal and spatial metadata from auslma granules
+        """
+
+        if self.file_name.startswith('IGNIS'):
+            tkn = re.search(r'^.*(\d{6}_\d{6}_\d{4}).*$', self.file_name)[1].split('_')  # i.e., 201016,142000,0600
+            secs = int(tkn[-1])  # i.e., 600 seconds
+            self.start_time = min(datetime.strptime('_'.join(tkn[0:2]), '%y%m%d_%H%M%S'),
+                                  self.start_time)
+            self.end_time = max(self.end_time, self.start_time + timedelta(seconds=secs - 1))
+        else:
+            raise ValueError(f"Unrecognized file type: {self.file_name}")
+
+        # Set to skip processing  *.qua.gz files in collection definition
+        # Process *.dat.gz files
+        # Extract lat/lon from file lines:
+        header_flag = False
+        with gzip.open(self.file_path, 'r') as f:
+            for line in f.readlines():
+                if header_flag:
+                    self.north, self.south = [max(self.north, float(line.split()[1])),
+                                              min(self.south, float(line.split()[1]))]
+                    self.east, self.west = [max(self.east, float(line.split()[2])),
+                                            min(self.west, float(line.split()[2]))]
+                if b'*** data ***' in line:
+                    header_flag = True
+
+        if self.north == self.south and self.east == self.west:
+            self.north, self.south, self.east, self.west = [self.north + 0.001, self.south - 0.001,
+                                                            self.east + 0.001, self.west - 0.001]
+
+        # For *.dat.gz with no data
+        if self.north == -90.0:
+            self.north, self.south, self.east, self.west = [36.72461000, 32.72461000, -84.64533000,
+                                                            -88.64533000]
+
+    def get_wnes_geometry(self, scale_factor=1.0, offset=0, **kwargs):
+        north, south, east, west = [round((x * scale_factor) + offset, 3) for x in
+                                    [self.north, self.south, self.east, self.west]]
+        return [self.convert_360_to_180(west), north, self.convert_360_to_180(east), south]
+
+    def get_temporal(self, time_variable_key='time', units_variable='units', scale_factor=1.0,
+                     offset=0,
+                     date_format='%Y-%m-%dT%H:%M:%SZ'):
+        start_date = self.start_time.strftime(date_format)
+        stop_date = self.end_time.strftime(date_format)
+        return start_date, stop_date
+
+    def get_metadata(self, ds_short_name, format='Binary', version='01', **kwargs):
+        start_date, stop_date = self.get_temporal(time_variable_key='lon',
+                                                  units_variable='time',
+                                                  date_format='%Y-%m-%dT%H:%M:%SZ')
+        data = dict()
+        data['ShortName'] = ds_short_name
+        data['GranuleUR'] = self.file_name
+        data['BeginningDateTime'], data['EndingDateTime'] = start_date, stop_date
+
+        gemetry_list = self.get_wnes_geometry()
+
+        data['WestBoundingCoordinate'], data['NorthBoundingCoordinate'], \
+            data['EastBoundingCoordinate'], data['SouthBoundingCoordinate'] = list(
+            str(x) for x in gemetry_list)
+        data['SizeMBDataGranule'] = str(round(self.get_file_size_megabytes(), 2))
+        data['checksum'] = self.get_checksum()
+        data['DataFormat'] = format
+        data['VersionId'] = version
+        return data
+
+
+if __name__ == '__main__':
+    print('Extracting auslma Metadata')
